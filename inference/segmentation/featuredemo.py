963f97eb7b6aa3b1d9e4fced37c0fdcf72830f93
E6B3BD <2677652756@qq.com>
2025-08-20 15:32:52 +0800

    # 移除单元测试demo

--- inference/segmentation/featuredemo.py
+++
@@ -1,6 +0,0 @@
-from ultralytics.nn.tasks import attempt_load_one_weight
-from ultralytics import YOLO
-
-model = YOLO(r'I:\python-Code\Supermarketsettlement\inference\models\Seg\best.pt')
-print(model.model.names)  # 查看类别
-print([k for k, v in model.model.named_modules()])  # 查看所有层名
